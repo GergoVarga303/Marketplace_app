efe0095d3297236ae0a2dfda69e83fab2b8b1abd
Gergo Varga <gergoo.varga@gmail.com>
2026-08-21 22:53:19 +0200

    added qucik testing for db connection (query route)

---
+++ app/query/route.ts
@@ -0,0 +1,20 @@
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+async function dbTest() {
+	const data = await sql`
+    SELECT *
+    FROM products;
+  `;
+
+	return data;
+}
+
+export async function GET() {
+  try {
+  	return Response.json(await dbTest());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
+}
